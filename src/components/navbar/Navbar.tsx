235c18c3f543a9738d322028f9f53be2a0a50d82
Andrey <adolfo.quiceno@correounivalle.edu.co>
2025-10-10 23:56:30 -0500

    style: Responsive for footer, implementation of burguer button for mobile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,15 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
 import linealLogo from "../../../public/lineal-logo.svg";
 
-
 const Navbar: FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar__container">
@@ -12,7 +17,8 @@ const Navbar: FC = () => {
           <img src={linealLogo} alt="Maraton Logo" />
         </Link>
 
-        <div className="navbar__links">
+        {/* Desktop Menu */}
+        <div className="navbar__links desktop-menu">
           <Link to="/" className="navbar__link">
             Inicio
           </Link>
@@ -24,9 +30,39 @@ const Navbar: FC = () => {
           </Link>
         </div>
 
-        <Link to="/login" className="navbar__auth-button">
-          INICIAR SESIÓN
-        </Link>
+        <div className="navbar__actions">
+          <Link to="/login" className="navbar__auth-button">
+            INICIAR SESIÓN
+          </Link>
+
+          {/* Hamburger Button */}
+          <button
+            className={`navbar__menu-button ${isMenuOpen ? "active" : ""}`}
+            onClick={toggleMenu}
+            aria-label="Toggle menu"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+
+        {/* Mobile Menu */}
+        <div className={`navbar__mobile-menu ${isMenuOpen ? "active" : ""}`}>
+          <Link to="/" className="navbar__link" onClick={toggleMenu}>
+            Inicio
+          </Link>
+          <Link to="/peliculas" className="navbar__link" onClick={toggleMenu}>
+            Catálogo
+          </Link>
+          <Link
+            to="/sobre-nosotros"
+            className="navbar__link"
+            onClick={toggleMenu}
+          >
+            Sobre nosotros
+          </Link>
+        </div>
       </div>
     </nav>
   );
